8b7e6dcbc74c1cfa53f13e3f5bfeee52fa46db25
kimbi91 <subscribefan@gmail.com>
2022-07-25 18:53:17 +0200

    update

--- src/main/java/hu/progmatic/progpanda/controller/FoodController.java
+++ src/main/java/hu/progmatic/progpanda/controller/FoodController.java
@@ -24,7 +24,7 @@ public class FoodController {
     }
 
     @GetMapping(value = {"/delivery"})
-    public String displayConditions() {
+    public String getDelivery() {
         return "delivery";
     }
 }
