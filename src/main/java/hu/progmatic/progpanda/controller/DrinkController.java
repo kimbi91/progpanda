a1234e7a2bb0838a85bc1fa0af88f846efdbca88
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-07-25 17:11:43 +0200

    bm: update

--- src/main/java/hu/progmatic/progpanda/controller/DrinkController.java
+++ src/main/java/hu/progmatic/progpanda/controller/DrinkController.java
@@ -1,2 +1,30 @@
-package hu.progmatic.progpanda.controller;public class DrinkController {
+package hu.progmatic.progpanda.controller;
+
+import hu.progmatic.progpanda.model.Drink;
+import hu.progmatic.progpanda.service.DrinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class DrinkController {
+    @Autowired
+    DrinkService drinkService;
+
+    /* @PostMapping("/drinks")
+    public String newDrink(Drink drink, Model model) {
+        drinkService.saveDrink(drink);
+        return "redirect:/drinks";
+    } */
+
+    @GetMapping(value = {"/", "/drinks"})
+    public String getDrinks(Model model) {
+        // List<Drink> drinks = drinkService.getAll();
+        // model.addAttribute("drinks", drinks);
+        return "drinks";
+    }
 }
--- src/main/java/hu/progmatic/progpanda/service/DrinkService.java
+++ src/main/java/hu/progmatic/progpanda/service/DrinkService.java
@@ -1,2 +1,34 @@
-package hu.progmatic.progpanda.service;public class DrinkService {
+package hu.progmatic.progpanda.service;
+
+import hu.progmatic.progpanda.model.Drink;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@Service
+public class DrinkService {
+    private int counter = 0;
+    private Map<Integer, Drink> drinks = new TreeMap<>();
+
+    public List<Drink> getAll() {
+        return new ArrayList<>(drinks.values());
+    }
+
+    public Drink getById(int id) {
+        return drinks.get(id);
+    }
+
+    public Drink saveDrink(Drink drink) {
+        if (drink.getId() == null) {
+            counter++;
+            drink.setId(counter);
+        }
+
+        drinks.put(drink.getId(), drink);
+
+        return drink;
+    }
 }
